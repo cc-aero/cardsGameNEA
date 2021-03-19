0c64559a6df72b7471da5d4bda19f9c713142fa3
Cameron <cameronlconnor@gmail.com>
2021-03-19 08:29:57 +0000

    Wrote all Password Tests in PasswordTests.java, also added LoginTests.java

--- src/test/java/PasswordTests.java
+++ src/test/java/PasswordTests.java
@@ -17,4 +17,22 @@ public class PasswordTests {
     void noCapsPw(){
         Assertions.assertEquals(false,Passwords.validatePassword("nocap1talshere!"));
     }
+
+    @Test
+    @DisplayName("Invalid Password No Number")
+    void noNumberPw(){
+        Assertions.assertEquals(false,Passwords.validatePassword("noNumbersHere!"));
+    }
+
+    @Test
+    @DisplayName("Invalid Password No Special Chars")
+    void noSpecialCharPw(){
+        Assertions.assertEquals(false,Passwords.validatePassword("noSpecialCh4rs"));
+    }
+
+    @Test
+    @DisplayName("Invalid Password Too Short")
+    void badLengthPw(){
+        Assertions.assertEquals(false,Passwords.validatePassword("tooshor"));
+    }
 }
