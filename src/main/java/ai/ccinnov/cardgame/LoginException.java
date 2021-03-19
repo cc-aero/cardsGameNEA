ffa1c38ae43705bb23a6a32b73df05864dc363f9
Cameron <cameronlconnor@gmail.com>
2021-03-19 08:59:52 +0000

    Made Login Exception

--- src/main/java/ai/ccinnov/cardgame/LoginException.java
+++ src/main/java/ai/ccinnov/cardgame/LoginException.java
@@ -1,5 +1,10 @@
 package ai.ccinnov.cardgame;
 
 public class LoginException extends Exception{
-
+    public LoginException(int reason) throws LoginException {
+        //Reasons:
+        //0: Class Error
+        //1: Invalid U/N or PW
+        throw new LoginException(reason);
+    }
 }
